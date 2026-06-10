feaac4f11303db0b4bec3266cc1ba29b6ac88735
Tuding071 <davidpenaranda071@gmail.com>
2026-06-10 15:55:03 +0800

    Update Browser.kt

--- Browser.kt
+++ Browser.kt
@@ -2225,7 +2225,7 @@ fun ContentLayer() {
                                                 val isPending     = pendingDeletions.containsKey(tabIndex)
                                                 val tabDomain     = getDomainName(tab.url)
                                                 LaunchedEffect(tab.url) { 
-                                                    delay(10)
+                                                    delay(100)
                                                     loadTabFavicon(tabDomain) 
                                                 }
                                                 val tabFav = tabFavicons[tabDomain]
